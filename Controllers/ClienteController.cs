478c6131caad2d3258999365d702ee67d5f27a45
Anderson dos Santos Bauer <andersondossantosbauer@gmail.com>
2024-11-21 21:32:55 -0300

    Aula-20-TDE

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -5,79 +5,62 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class ClienteController : ControllerBase
 {
-    private static List<Cliente> clientes = new List<Cliente>();
+    private readonly AppDbContext _context;
+
+    public ClienteController(AppDbContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
     public ActionResult<List<Cliente>> GetAll()
     {
-        return clientes;
+        return _context.Clientes.ToList();
     }
 
     [HttpGet("{id}")]
     public ActionResult<Cliente> GetById(int id)
     {
-        Cliente clienteEncontrado = null;
-        foreach(var cliente in clientes)
-        {
-            if(cliente.Id == id)
-            {
-                clienteEncontrado = cliente;
-                break;
-            }
-        }
+        var clienteEncontrado = _context.Clientes.Find(id);
         if(clienteEncontrado == null)
-        {
             return NotFound();
-        }
+
         return clienteEncontrado;
     }
 
     [HttpPost]
     public ActionResult Post(Cliente cliente)
     {
-        clientes.Add(cliente);
+        _context.Clientes.Add(cliente);
+        _context.SaveChanges();
         return Created();
     }
     
     [HttpPut("{id}")]
     public ActionResult Put(int id, Cliente clienteAtualizado)
     {
-        Cliente clienteEncontrado = null;
-        foreach(var cliente in clientes)
-        {
-            if(cliente.Id == id)
-            {
-                clienteEncontrado = cliente;
-                break;
-            }
-        }
+        var clienteEncontrado = _context.Clientes.Find(id);
+        
         if(clienteEncontrado == null)
-        {
             return NotFound();
-        }
+
         clienteEncontrado.Nome = clienteAtualizado.Nome;
         clienteEncontrado.Contato = clienteAtualizado.Contato;
 
+        _context.SaveChanges();
         return NoContent();
     }
 
     [HttpDelete("{id}")]
     public ActionResult Delete(int id)
     {
-        Cliente clienteEncontrado = null;
-        foreach(var cliente in clientes)
-        {
-            if(cliente.Id == id)
-            {
-                clienteEncontrado = cliente;
-                break;
-            }
-        }
+        var clienteEncontrado = _context.Clientes.Find(id);
+        
         if(clienteEncontrado == null)
-        {
             return NotFound();
-        }
-        clientes.Remove(clienteEncontrado);
+        
+        _context.Clientes.Remove(clienteEncontrado);
+        _context.SaveChanges();
         return NoContent();
     }
 }
